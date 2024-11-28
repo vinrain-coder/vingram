d8d215cf0fad9688e99ac0d6d81e1c514105c3a3
vinrain <143264723+vinrain-coder@users.noreply.github.com>
2024-11-28 13:21:41 +0000

    database setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
