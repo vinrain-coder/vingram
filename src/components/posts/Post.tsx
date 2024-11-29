661391b60e03b90df96c1a7974170be3ab4f78f7
vinrain <143264723+vinrain-coder@users.noreply.github.com>
2024-11-29 10:00:45 +0000

    Optimize post fetching

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,5 +1,8 @@
+//@ts-nocheck
 import { Post as PostData } from "@prisma/client";
 import Link from "next/link";
+import UserAvatar from "../UserAvatar";
+import formatRelativeDate from "@/lib/utils";
 
 interface PostProps {
   post: PostData;
@@ -12,8 +15,22 @@ export default function Post({ post }: PostProps) {
         <Link href={`/users/${post.user.username}`}>
           <UserAvatar avatarUrl={post.user.avatar} />
         </Link>
-        
+        <div>
+          <Link
+            href={`/users/${post.user.username}`}
+            className="block font-medium hover:underline"
+          >
+            {post.user.displayName}
+          </Link>
+          <Link
+            href={`/posts/${post.id}`}
+            className="block text-sm text-muted-foreground hover-underline"
+          >
+            {formatRelativeDate(post.createdAt)}
+          </Link>
+        </div>
       </div>
+      <div className="whitespace-pre-line break-words">{post.content}</div>
     </article>
   );
 }
