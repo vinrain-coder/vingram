b926698961960b5a3d7a1a516a76a8f396acc98f
vinrain <143264723+vinrain-coder@users.noreply.github.com>
2024-11-29 13:18:03 +0000

    Implement useInfiniteQuery

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -26,7 +26,7 @@ export default function Post({ post }: PostProps) {
             href={`/posts/${post.id}`}
             className="block text-sm text-muted-foreground hover-underline"
           >
-            {/* {formatRelativeDate(post.createdAt)} */}
+            {formatRelativeDate(post.createdAt)}
           </Link>
         </div>
       </div>
