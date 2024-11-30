b2d772b0f69d80e96d63ab86b8a9a83d61e6281f
vinrain <143264723+vinrain-coder@users.noreply.github.com>
2024-11-30 15:08:11 +0000

    Fix tooltip

--- src/components/UserTooltip.tsx
+++ src/components/UserTooltip.tsx
@@ -1,67 +1,57 @@
-"use client";
-
-import { useSession } from "@/app/(main)/SessionProvider";
-import { FollowerInfo, UserData } from "@/lib/types";
-import { PropsWithChildren } from "react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "./ui/tooltip";
 import Link from "next/link";
-import UserAvatar from "./UserAvatar";
-import FollowButton from "./FollowButton";
-import Linkify from "./Linkify";
-import FollowerCount from "./FollowerCount";
+import { LinkIt, LinkItUrl } from "react-linkify-it";
+import UserLinkWithTooltip from "./UserLinkWithTooltip";
 
-interface UserToolTipProps extends PropsWithChildren {
-  user: UserData;
+interface LinkifyProps {
+  children: React.ReactNode;
 }
 
-export default function UserToolTip({ children, user }: UserToolTipProps) {
-  const { user: loggedInUser } = useSession();
-
-  const followerState: FollowerInfo = {
-    followers: user._count.followers,
-    isFollowedByUser: !!user.followers.some(
-      ({ followerId }) => followerId === loggedInUser.id
-    ),
-  };
-
+export default function Linkify({ children }: LinkifyProps) {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>{children}</TooltipTrigger>
-        <TooltipContent>
-          <div className="flex max-w-80 flex-col gap-3 break-words px-1 py-2.5 md:min-w-52">
-            <div className="flex items-center justify-between gap-2">
-              <Link href={`/users/${user.username}`}>
-                <UserAvatar size={70} avatarUrl={user.avatarUrl} />
-              </Link>
-              {loggedInUser.id !== user.id && (
-                <FollowButton userId={user.id} initialState={followerState} />
-              )}
-            </div>
-            <div className="">
-              <Link href={`/users/${user.username}`}>
-                <div className="text-lg font-semibold hover:underline">
-                  {user.displayName}
-                </div>
-                <div className="text-muted-foreground">@{user.username}</div>
-              </Link>
-            </div>
-            {user.bio && (
-              <Linkify>
-                <div className="line-clamp-4 whitespace-pre-line">
-                  {user.bio}
-                </div>
-              </Linkify>
-            )}
-            <FollowerCount userId={user.id} initialState={followerState} />
-          </div>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <LinkifyUsername>
+      <LinkifyHashtag>
+        <LinkifyUrl>{children}</LinkifyUrl>
+      </LinkifyHashtag>
+    </LinkifyUsername>
+  );
+}
+
+function LinkifyUrl({ children }: LinkifyProps) {
+  return (
+    <LinkItUrl className="text-primary hover:underline">{children}</LinkItUrl>
+  );
+}
+
+function LinkifyUsername({ children }: LinkifyProps) {
+  return (
+    <LinkIt
+      regex={/(@[a-zA-Z0-9_-]+)/}
+      component={(match, key) => (
+        <UserLinkWithTooltip key={key} username={match.slice(1)}>
+          {match}
+        </UserLinkWithTooltip>
+      )}
+    >
+      {children}
+    </LinkIt>
+  );
+}
+
+function LinkifyHashtag({ children }: LinkifyProps) {
+  return (
+    <LinkIt
+      regex={/(#[a-zA-Z0-9]+)/}
+      component={(match, key) => (
+        <Link
+          key={key}
+          href={`/hashtag/${match.slice(1)}`}
+          className="text-primary hover:underline"
+        >
+          {match}
+        </Link>
+      )}
+    >
+      {children}
+    </LinkIt>
   );
 }
